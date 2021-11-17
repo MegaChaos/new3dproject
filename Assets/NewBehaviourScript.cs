008c702e85286f3e7584befaacc06fc21c224664
Frankie Miller <fmiller2021@elam.co.uk>
2021-11-17 10:40:34 +0000

    Changed size of platform

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+     
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.parent = transform;
+        }
+    }
+
+    
+    
+    {
+        
+    }
+}
